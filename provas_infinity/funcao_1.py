8c50c2fdcddba7eac10839c9c93b1d28854ea6ab
José Macêdo <josemaceowork@gmail.com>
2025-07-01 11:13:24 -0300

    Prova de funções 1 infinity

---
+++ provas_infinity/funcao_1.py
@@ -0,0 +1,9 @@
+# [PYIA-A04] Crie uma função chamada media que receberá três números como argumentos. Esta função deve calcular a média aritmética desses três números. Para fazer isso, some os três números e, em seguida, divida o resultado por três. Por fim, a função deve retornar o valor da média aritmética calculada.
+
+
+
+def media(a= 1, b= 2, c= 3):
+    return (a + b + c) // 3
+
+
+print(media())
