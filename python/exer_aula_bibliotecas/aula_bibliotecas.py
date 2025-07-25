93460ec11290edfd8411622e726ddcc40ce1ef49
Jose Macedo <josemacedowork@gmail.com>
2025-07-24 21:57:21 -0300

    Inicio dos exercicios de bibliotecas

---
+++ python/exer_aula_bibliotecas/aula_bibliotecas.py
@@ -0,0 +1,36 @@
+from func_exerc_bibliotecas import *
+
+
+
+# ATIVIDADE PRÁTICA 3
+
+# Crie um programa que permite ao usuário calcular a
+# área e o perímetro de formas geométricas simples,
+# como quadrados, retângulos e círculos. Use funções
+# matemáticas do módulo math para realizar os cálculos.
+
+
+
+
+
+
+# ATIVIDADE PRÁTICA 4
+
+# Desenvolva um jogo de adivinhação em que o programa
+# escolhe um número aleatório e desafia o jogador a
+# adivinhá-lo. Forneça dicas ao jogador, indicando se o
+# número é maior ou menor do que a adivinhação atual.
+
+
+
+
+
+
+# ATIVIDADE PRÁTICA 5
+
+# Desenvolva um programa que permita ao usuário
+# converter entre diferentes unidades de medida, como
+# metros para pés, quilogramas para libras, ou Celsius
+# para Fahrenheit. Use módulos que contenham funções
+
+# de conversão.
---
+++ python/exer_aula_bibliotecas/func_exerc_bibliotecas.py
@@ -0,0 +1,32 @@
+from math import *
+
+def calcular_area_quadrado():
+    ...
+
+def calcular_area_retangulo():
+    ...
+    
+def calcular_area_circulo():
+    ...
+
+
+
+
+def calcular_perimetro_quadrado():
+    ...
+
+def calcular_perimetro_retangulo():
+    ...
+
+def calcular_perimetro_circulo():
+    ...
+
+
+
+
+
+
+
+
+
+
