f37b76ee3e2e508478c0e256c52d613050c5fc03
José Macêdo <josemaceowork@gmail.com>
2025-08-03 09:21:53 -0300

    continuação atividade 2 aula 7

--- python/revisao_aula_8/atividade_2.py
+++ python/revisao_aula_8/atividade_2.py
@@ -31,25 +31,51 @@ def add_produto():
     }
 
     lista_produtos.append(produto)
-    return (f'Produto "{produto['nome']}", cadastrado com sucesso')
+    return ( f"Produto {produto['nome']}, cadastrado com sucesso")
+
+def listar_produtos():
+        # Percorrer a lista de lista_produtos
+    if len(lista_produtos) == 0:
+        return 'Lista de lista_produtos vazia'
+
+    for produto in lista_produtos:
+        # status = 'Concluído' if tarefa['concluido'] else 'Pendente'
+
+        print(f"ID: {lista_produtos.index(produto)}")
+        print(f"Nome: {produto['nome']}")
+        print(f"Preço: {produto['preco']}")
+        print(f"Estoque: {produto['estoque']}")
+        print(10 * '-')
 
 
 
-def lista
+
+
 print(add_produto())
-print(lista_produtos)
-
-
-
-
+listar_produtos()
 
 
 def remove_produto():
-    ...
+    excluir_id = int(input('Digite o "ID" o produtoque você quer excluir: '))
+    lista_produtos.pop(excluir_id)
+    return f'A tarefa com o ID: {excluir_id} foi excluida com sucesso. '
 
 def update_produto():
     ...
 
 
 
+# while True:
+#     opcao = input('Digite: \n[1]-Cadastrar um produto \n[2]-Remover um produto \n[3]-Atualizar um produto \n[0]-Encerrar o programa')
 
+#     if opcao == 0:
+#         break
+
+#     elif opcao == 1:
+#         ...
+
+#     elif opcao == 2:
+#         ...
+
+#     elif opcao == 3:
+#         ...
